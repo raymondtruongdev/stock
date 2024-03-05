a005df3cee2ee8ddd42b805bb269688f151f67cb
raymond <raymondtruongdev@gmail.com>
2024-03-05 23:24:57 +0700

    initialize

---
+++ android/app/src/main/kotlin/com/example/stock_chart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stock_chart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:stock_chart/themes/light_mode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stock Chart',
+      theme: lighMode,
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Container(
+          color: Colors.blue,
+          child: const Text('data'),
+        ),
+      ),
+    );
+  }
+}
